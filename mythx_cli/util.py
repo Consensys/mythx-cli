fd2a3dad1cb885bb7a140ed22436c0b03be4dece
Dominik Muhs <dmuhs@protonmail.ch>
2019-09-11 17:00:35 +0200

    Simplify solc source map resolution

--- mythx_cli/util.py
+++ mythx_cli/util.py
@@ -8,13 +8,4 @@ def get_source_location_by_offset(source, offset):
     :return: The line number
     """
 
-    overall = 0
-    line_ctr = 0
-    for line in source.split("\n"):
-        line_ctr += 1
-        overall += len(line)
-        if overall >= offset:
-            return line_ctr
-    raise click.exceptions.ClickException(
-        "Error finding the source location for offset {} - max overall {} reached".format(offset, overall)
-    )
+    return source.encode("utf-8")[0:offset].count("\n".encode("utf-8")) + 1
