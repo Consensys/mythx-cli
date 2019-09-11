a4da013a013d536122163a226d22a6eb8d43948c
Dominik Muhs <dmuhs@protonmail.ch>
2019-09-11 17:18:02 +0200

    Add version tests

--- Makefile
+++ Makefile
@@ -54,7 +54,7 @@ lint: ## check style with flake8
 	flake8 mythx_cli tests
 
 test: ## run tests quickly with the default Python
-	py.test
+	py.test -vv
 
 test-all: ## run tests on every Python version with tox
 	tox
---
+++ tests/test_version.py
@@ -0,0 +1,39 @@
+from click.testing import CliRunner
+from mythx_cli.cli import cli
+from unittest.mock import patch
+from mythx_models.response import VersionResponse
+import json
+
+
+VERSION_RESPONSE_OBJ = VersionResponse.from_dict({
+  "api": "v1.4.34.4",
+  "maru": "0.5.3",
+  "mythril": "0.21.14",
+  "harvey": "0.0.33",
+  "hash": "6e0035da873e809e90eab4665e3d19d6"
+})
+
+VERSION_RESPONSE_SIMPLE = """API: v1.4.34.4
+Harvey: 0.0.33
+Maru: 0.5.3
+Mythril: 0.21.14
+Hashed: 6e0035da873e809e90eab4665e3d19d6
+"""
+
+
+def test_version_simple():
+    runner = CliRunner()
+    with patch("pythx.Client.version") as version_patch:
+        version_patch.return_value = VERSION_RESPONSE_OBJ
+        result = runner.invoke(cli, ["version"])
+        assert result.exit_code == 0
+        assert result.output == VERSION_RESPONSE_SIMPLE
+
+
+def test_version_json():
+    runner = CliRunner()
+    with patch("pythx.Client.version") as version_patch:
+        version_patch.return_value = VERSION_RESPONSE_OBJ
+        result = runner.invoke(cli, ["--format", "json", "version"])
+        assert result.exit_code == 0
+        assert json.loads(result.output) == VERSION_RESPONSE_OBJ.to_dict()
