3e187e5485797943caf547e837dd1c2c4acd6b76
Dominik Muhs <dmuhs@protonmail.ch>
2019-09-15 12:19:11 +0200

    Add docs for JSON formatters

--- mythx_cli/formatter/json.py
+++ mythx_cli/formatter/json.py
@@ -15,26 +15,36 @@ from mythx_cli.formatter.base import BaseFormatter
 class JSONFormatter(BaseFormatter):
     @staticmethod
     def format_analysis_list(resp: AnalysisListResponse) -> str:
+        """Format an analysis list response as compressed JSON."""
+
         return resp.to_json()
 
     @staticmethod
     def format_analysis_status(resp: AnalysisStatusResponse) -> str:
+        """Format an analysis status response as compressed JSON."""
+
         return resp.to_json()
 
     @staticmethod
     def format_detected_issues(
         resp: DetectedIssuesResponse, inp: AnalysisInputResponse
     ) -> str:
+        """Format an issue report response as compressed JSON."""
+
         return resp.to_json()
 
     @staticmethod
     def format_version(resp: VersionResponse) -> str:
+        """Format a version response as compressed JSON."""
+
         return resp.to_json()
 
 
 class PrettyJSONFormatter(BaseFormatter):
     @staticmethod
     def _print_as_json(obj):
+        """Pretty-print the given object's JSON representation."""
+
         json_args = {"indent": 2, "sort_keys": True}
         if type(obj) == DetectedIssuesResponse:
             return json.dumps(obj.to_dict(as_list=True), **json_args)
@@ -42,16 +52,24 @@ class PrettyJSONFormatter(BaseFormatter):
 
     @staticmethod
     def format_analysis_list(obj: AnalysisListResponse) -> str:
+        """Format an analysis list response as pretty-printed JSON."""
+
         return PrettyJSONFormatter._print_as_json(obj)
 
     @staticmethod
     def format_analysis_status(obj: AnalysisStatusResponse) -> str:
+        """Format an analysis status response as pretty-printed JSON."""
+
         return PrettyJSONFormatter._print_as_json(obj)
 
     @staticmethod
     def format_detected_issues(obj: DetectedIssuesResponse, inp: AnalysisInputResponse):
+        """Format an issue report response as pretty-printed JSON."""
+
         return PrettyJSONFormatter._print_as_json(obj)
 
     @staticmethod
     def format_version(obj: VersionResponse):
+        """Format a version response as pretty-printed JSON."""
+
         return PrettyJSONFormatter._print_as_json(obj)
