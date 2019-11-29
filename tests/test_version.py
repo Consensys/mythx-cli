7c2da5256ee45686ee2b17e20f5884f6ce6ee7b2
Dominik Muhs <dmuhs@protonmail.ch>
2019-11-29 16:06:11 +0100

    Refactor version tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -4,46 +4,47 @@ from unittest.mock import patch
 from click.testing import CliRunner
 
 from mythx_cli.cli import cli
+from mythx_models.response import VersionResponse
+from .common import get_test_case
 
-from .testdata import (
-    VERSION_RESPONSE_OBJ,
-    VERSION_RESPONSE_SIMPLE,
-    VERSION_RESPONSE_TABLE,
-)
+
+VERSION_RESPONSE = get_test_case("testdata/version-response.json", VersionResponse)
+VERSION_SIMPLE = get_test_case("testdata/version-simple.txt", raw=True)
+VERSION_TABLE = get_test_case("testdata/version-table.txt", raw=True)
 
 
 def test_version_tabular():
     runner = CliRunner()
     with patch("pythx.Client.version") as version_patch:
-        version_patch.return_value = VERSION_RESPONSE_OBJ
+        version_patch.return_value = VERSION_RESPONSE
         result = runner.invoke(cli, ["version"])
         assert result.exit_code == 0
-        assert result.output == VERSION_RESPONSE_TABLE
+        assert result.output == VERSION_TABLE
 
 
 def test_version_json():
     runner = CliRunner()
     with patch("pythx.Client.version") as version_patch:
-        version_patch.return_value = VERSION_RESPONSE_OBJ
+        version_patch.return_value = VERSION_RESPONSE
         result = runner.invoke(cli, ["--format", "json", "version"])
         assert result.exit_code == 0
-        assert json.loads(result.output) == VERSION_RESPONSE_OBJ.to_dict()
+        assert json.loads(result.output) == VERSION_RESPONSE.to_dict()
 
 
 def test_version_json_pretty():
     runner = CliRunner()
     with patch("pythx.Client.version") as version_patch:
-        version_patch.return_value = VERSION_RESPONSE_OBJ
+        version_patch.return_value = VERSION_RESPONSE
         result = runner.invoke(cli, ["--format", "json-pretty", "version"])
         assert result.exit_code == 0
-        assert json.loads(result.output) == VERSION_RESPONSE_OBJ.to_dict()
+        assert json.loads(result.output) == VERSION_RESPONSE.to_dict()
 
 
 def test_version_simple():
     runner = CliRunner()
     with patch("pythx.Client.version") as version_patch:
-        version_patch.return_value = VERSION_RESPONSE_OBJ
+        version_patch.return_value = VERSION_RESPONSE
         result = runner.invoke(cli, ["--format", "simple", "version"])
         assert result.exit_code == 0
         print(result.output)
-        assert result.output == VERSION_RESPONSE_SIMPLE
+        assert result.output == VERSION_SIMPLE
