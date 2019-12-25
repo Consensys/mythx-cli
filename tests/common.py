b79077507cdb63d8349fb706cdac40607568cfac
Dominik Muhs <dmuhs@protonmail.ch>
2019-12-25 15:57:21 +0100

    Introduce test context manager

--- tests/common.py
+++ tests/common.py
@@ -1,5 +1,14 @@
 import json
+from contextlib import contextmanager
+from copy import deepcopy
 from pathlib import Path
+from unittest.mock import patch
+
+from mythx_models.response import (
+    AnalysisInputResponse,
+    AnalysisSubmissionResponse,
+    DetectedIssuesResponse,
+)
 
 
 def get_test_case(path: str, obj=None, raw=False):
@@ -11,3 +20,35 @@ def get_test_case(path: str, obj=None, raw=False):
     if obj is None:
         return dict_data
     return obj.from_dict(dict_data)
+
+
+@contextmanager
+def mock_context(submission_response=None, issues_response=None, input_response=None):
+    with patch("pythx.Client.analyze") as analyze_patch, patch(
+        "pythx.Client.analysis_ready"
+    ) as ready_patch, patch("pythx.Client.report") as report_patch, patch(
+        "pythx.Client.request_by_uuid"
+    ) as input_patch, patch(
+        "solcx.compile_source"
+    ) as compile_patch:
+        analyze_patch.return_value = submission_response or get_test_case(
+            "testdata/analysis-submission-response.json", AnalysisSubmissionResponse
+        )
+        ready_patch.return_value = True
+        report_patch.return_value = deepcopy(issues_response) or get_test_case(
+            "testdata/detected-issues-response.json", DetectedIssuesResponse
+        )
+        input_patch.return_value = input_response or get_test_case(
+            "testdata/analysis-input-response.json", AnalysisInputResponse
+        )
+        compile_patch.return_value = {
+            "contract": {
+                "abi": "test",
+                "ast": "test",
+                "bin": "test",
+                "bin-runtime": "test",
+                "srcmap": "test",
+                "srcmap-runtime": "test",
+            }
+        }
+        yield analyze_patch, ready_patch, report_patch, input_patch, compile_patch
--- tests/test_analyze_bytecode.py
+++ tests/test_analyze_bytecode.py
@@ -1,5 +1,4 @@
 from copy import deepcopy
-from unittest.mock import patch
 
 from click.testing import CliRunner
 
@@ -11,7 +10,7 @@ from mythx_models.response import (
     Severity,
 )
 
-from .common import get_test_case
+from .common import get_test_case, mock_context
 
 FORMAT_ERROR = "Could not interpret argument lolwut as bytecode or Solidity file"
 SUBMISSION_RESPONSE = get_test_case(
@@ -27,16 +26,7 @@ INPUT_RESPONSE = get_test_case(
 
 def test_bytecode_analyze():
     runner = CliRunner()
-    with patch("pythx.Client.analyze") as analyze_patch, patch(
-        "pythx.Client.analysis_ready"
-    ) as ready_patch, patch("pythx.Client.report") as report_patch, patch(
-        "pythx.Client.request_by_uuid"
-    ) as input_patch:
-        analyze_patch.return_value = SUBMISSION_RESPONSE
-        ready_patch.return_value = True
-        report_patch.return_value = deepcopy(ISSUES_RESPONSE)
-        input_patch.return_value = INPUT_RESPONSE
-
+    with mock_context():
         result = runner.invoke(cli, ["analyze", "0xfe"])
         assert result.exit_code == 0
         assert INPUT_RESPONSE.source_list[0] in result.output
@@ -44,38 +34,20 @@ def test_bytecode_analyze():
 
 def test_bytecode_analyze_file_output():
     runner = CliRunner()
-    with patch("pythx.Client.analyze") as analyze_patch, patch(
-        "pythx.Client.analysis_ready"
-    ) as ready_patch, patch("pythx.Client.report") as report_patch, patch(
-        "pythx.Client.request_by_uuid"
-    ) as input_patch:
-        analyze_patch.return_value = SUBMISSION_RESPONSE
-        ready_patch.return_value = True
-        report_patch.return_value = deepcopy(ISSUES_RESPONSE)
-        input_patch.return_value = INPUT_RESPONSE
-
-        with runner.isolated_filesystem():
-            result = runner.invoke(cli, ["--output", "test.log", "analyze", "0xfe"])
-            assert result.exit_code == 0
-            with open("test.log") as f:
-                assert INPUT_RESPONSE.source_list[0] in f.read()
+    with mock_context(), runner.isolated_filesystem():
+        result = runner.invoke(cli, ["--output", "test.log", "analyze", "0xfe"])
+        assert result.exit_code == 0
+        with open("test.log") as f:
+            assert INPUT_RESPONSE.source_list[0] in f.read()
 
 
 def test_bytecode_analyze_ci():
     runner = CliRunner()
-    with patch("pythx.Client.analyze") as analyze_patch, patch(
-        "pythx.Client.analysis_ready"
-    ) as ready_patch, patch("pythx.Client.report") as report_patch, patch(
-        "pythx.Client.request_by_uuid"
-    ) as input_patch:
+    with mock_context() as patches:
         # set up high-severity issue
         issues_resp = deepcopy(ISSUES_RESPONSE)
         issues_resp.issue_reports[0].issues[0].severity = Severity.HIGH
-
-        analyze_patch.return_value = SUBMISSION_RESPONSE
-        ready_patch.return_value = True
-        report_patch.return_value = issues_resp
-        input_patch.return_value = INPUT_RESPONSE
+        patches[2].return_value = issues_resp
 
         result = runner.invoke(cli, ["--ci", "analyze", "0xfe"])
         assert result.exit_code == 1
@@ -84,16 +56,7 @@ def test_bytecode_analyze_ci():
 
 def test_bytecode_analyze_blacklist():
     runner = CliRunner()
-    with patch("pythx.Client.analyze") as analyze_patch, patch(
-        "pythx.Client.analysis_ready"
-    ) as ready_patch, patch("pythx.Client.report") as report_patch, patch(
-        "pythx.Client.request_by_uuid"
-    ) as input_patch:
-        analyze_patch.return_value = SUBMISSION_RESPONSE
-        ready_patch.return_value = True
-        report_patch.return_value = deepcopy(ISSUES_RESPONSE)
-        input_patch.return_value = INPUT_RESPONSE
-
+    with mock_context():
         result = runner.invoke(cli, ["analyze", "--swc-blacklist", "SWC-110", "0xfe"])
         assert result.exit_code == 0
         assert "Assert Violation" not in result.output
@@ -105,16 +68,7 @@ def test_bytecode_analyze_blacklist():
 
 def test_bytecode_analyze_filter():
     runner = CliRunner()
-    with patch("pythx.Client.analyze") as analyze_patch, patch(
-        "pythx.Client.analysis_ready"
-    ) as ready_patch, patch("pythx.Client.report") as report_patch, patch(
-        "pythx.Client.request_by_uuid"
-    ) as input_patch:
-        analyze_patch.return_value = SUBMISSION_RESPONSE
-        ready_patch.return_value = True
-        report_patch.return_value = deepcopy(ISSUES_RESPONSE)
-        input_patch.return_value = INPUT_RESPONSE
-
+    with mock_context():
         result = runner.invoke(cli, ["analyze", "--min-severity", "high", "0xfe"])
         assert result.exit_code == 0
         assert "Assert Violation" not in result.output
@@ -126,16 +80,7 @@ def test_bytecode_analyze_filter():
 
 def test_bytecode_analyze_invalid():
     runner = CliRunner()
-    with patch("pythx.Client.analyze") as analyze_patch, patch(
-        "pythx.Client.analysis_ready"
-    ) as ready_patch, patch("pythx.Client.report") as report_patch, patch(
-        "pythx.Client.request_by_uuid"
-    ) as input_patch:
-        analyze_patch.return_value = SUBMISSION_RESPONSE
-        ready_patch.return_value = True
-        report_patch.return_value = deepcopy(ISSUES_RESPONSE)
-        input_patch.return_value = INPUT_RESPONSE
-
+    with mock_context():
         result = runner.invoke(cli, ["analyze", "lolwut"])
         assert result.exit_code == 2
         assert FORMAT_ERROR in result.output
