b576d7c82a69c61172233b2f044ae71bd7c4722f
Dominik Muhs <dmuhs@protonmail.ch>
2020-01-13 18:11:57 +0100

    Add sonarqube formatter

--- mythx_cli/formatter/__init__.py
+++ mythx_cli/formatter/__init__.py
@@ -1,12 +1,14 @@
 from .json import JSONFormatter, PrettyJSONFormatter
 from .simple_stdout import SimpleFormatter
 from .tabular import TabularFormatter
+from .sonarqube import SonarQubeFormatter
 
 FORMAT_RESOLVER = {
     "simple": SimpleFormatter(),
     "json": JSONFormatter(),
     "json-pretty": PrettyJSONFormatter(),
     "table": TabularFormatter(),
+    "sonar": SonarQubeFormatter(),
 }
 
-__all__ = [JSONFormatter, PrettyJSONFormatter, SimpleFormatter, TabularFormatter]
+__all__ = [JSONFormatter, PrettyJSONFormatter, SimpleFormatter, TabularFormatter, SonarQubeFormatter]
