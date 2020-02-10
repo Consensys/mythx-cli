c41dc0d65842ca46b39ed784f100b44d482395f6
Dominik Muhs <dmuhs@protonmail.ch>
2020-02-10 18:44:14 +0100

    Add more type hints to payload generators

--- mythx_cli/payload/bytecode.py
+++ mythx_cli/payload/bytecode.py
@@ -1,8 +1,10 @@
 """This module contains functions to generate bytecode-only analysis request
 payloads."""
 
+from typing import Dict
 
-def generate_bytecode_payload(code):
+
+def generate_bytecode_payload(code: str) -> Dict[str, str]:
     """Generate a payload containing only the creation bytecode.
 
     :param code: The creation bytecode as hex string starting with :code:`0x`
