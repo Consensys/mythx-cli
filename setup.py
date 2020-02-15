943d77f9dbd4d069e284a903293cc742130fb190
Dominik Muhs <dmuhs@protonmail.ch>
2020-02-15 14:08:40 +0100

    Specify long description cotnent type

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + "\n\n" + history,
+    long_description_content_type="text/x-rst",
     include_package_data=True,
     keywords="mythx-cli",
     name="mythx_cli",
