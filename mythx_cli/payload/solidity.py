442534c9d518ac7a3a92c8cf257efa162f8d5567
Dominik Muhs <dmuhs@protonmail.ch>
2020-04-08 16:32:52 +0200

    Switch to solc JSON stdin compilation

--- mythx_cli/payload/solidity.py
+++ mythx_cli/payload/solidity.py
@@ -3,13 +3,13 @@
 import logging
 import re
 from pathlib import Path
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, Optional, Tuple
 
 import click
 import solcx
 import solcx.exceptions
 
-from mythx_cli.payload.util import patch_solc_bytecode, set_srcmap_indices
+from mythx_cli.payload.util import patch_solc_bytecode
 
 LOGGER = logging.getLogger("mythx-cli")
 PRAGMA_PATTERN = r"pragma solidity [\^<>=]*(\d+\.\d+\.\d+);"
@@ -18,7 +18,7 @@ PRAGMA_PATTERN = r"pragma solidity [\^<>=]*(\d+\.\d+\.\d+);"
 def generate_solidity_payload(
     file: str,
     version: Optional[str],
-    contracts: List[str] = None,
+    contract: str = None,
     remappings: Tuple[str] = None,
 ) -> Dict:
     """Generate a MythX analysis request from a given Solidity file.
@@ -40,13 +40,15 @@ def generate_solidity_payload(
 
     :param file: The path pointing towards the Solidity file
     :param version: The solc version to use for compilation
-    :param contracts: The contract name(s) to submit
+    :param contract: The contract name(s) to submit
     :param remappings: Import remappings to pass to solcx
     :return: The payload dictionary to be sent to MythX
     """
 
     with open(file) as f:
-        solc_version = re.findall(PRAGMA_PATTERN, f.read())
+        source = f.read()
+
+    solc_version = re.findall(PRAGMA_PATTERN, source)
     LOGGER.debug(f"solc version matches in {file}: {solc_version}")
 
     if not (solc_version or version):
@@ -70,22 +72,31 @@ def generate_solidity_payload(
     try:
         cwd = str(Path.cwd().absolute())
         LOGGER.debug(f"Compiling {file} under allowed path {cwd}")
-        result = solcx.compile_files(
-            [file],
-            output_values=(
-                "abi",
-                "ast",
-                "bin",
-                "bin-runtime",
-                "srcmap",
-                "srcmap-runtime",
-            ),
-            import_remappings=[r.format(pwd=cwd) for r in remappings]
-            or [
-                f"openzeppelin-solidity/={cwd}/node_modules/openzeppelin-solidity/",
-                f"openzeppelin-zos/={cwd}/node_modules/openzeppelin-zos/",
-                f"zos-lib/={cwd}/node_modules/zos-lib/",
-            ],
+        result = solcx.compile_standard(
+            input_data={
+                "language": "Solidity",
+                "sources": {file: {"urls": [file]}},
+                "settings": {
+                    "remappings": [r.format(pwd=cwd) for r in remappings]
+                    or [
+                        f"openzeppelin-solidity/={cwd}/node_modules/openzeppelin-solidity/",
+                        f"openzeppelin-zos/={cwd}/node_modules/openzeppelin-zos/",
+                        f"zos-lib/={cwd}/node_modules/zos-lib/",
+                    ],
+                    "outputSelection": {
+                        "*": {
+                            "*": [
+                                "evm.bytecode.object",
+                                "evm.bytecode.sourceMap",
+                                "evm.deployedBytecode.object",
+                                "evm.deployedBytecode.sourceMap",
+                            ],
+                            "": ["ast"],
+                        }
+                    },
+                    "optimizer": {"enabled": True, "runs": 200},
+                },
+            },
             allow_paths=cwd,
         )
     except solcx.exceptions.SolcError as e:
@@ -93,48 +104,74 @@ def generate_solidity_payload(
             f"Error compiling source with solc {solc_version}: {e}"
         )
 
-    # sanitize solcx keys
-    new_result = {}
-    for key, value in result.items():
-        new_key = key.split(":")[1]
-        LOGGER.debug(f"Sanitizing solc key {key} -> {new_key}")
-        new_result[new_key] = value
-    result = new_result
+    compiled_sources = result.get("sources", {})
 
-    payload = {"sources": {}, "solc_version": solc_version, "source_list": []}
+    payload = {
+        "sources": {},
+        "solc_version": solc_version,
+        "main_source": file,
+        "source_list": [None] * len(compiled_sources),
+    }
 
+    for file_path, file_data in compiled_sources.items():
+        # fill source list entry
+        payload["source_list"][file_data.get("id")] = file_path
+
+        payload_dict = payload["sources"][file_path] = {}
+
+        # add AST for file if it's present
+        ast = file_data.get("ast")
+        if ast:
+            payload_dict["ast"] = ast
+
+        # add source from file path
+        with open(file_path) as source_f:
+            payload_dict["source"] = source_f.read()
+
+    if contract:
+        try:
+            # if contract specified, set its bytecode and source mapping
+            payload["contract_name"] = contract
+            payload["bytecode"] = patch_solc_bytecode(
+                result["contracts"][file][contract]["evm"]["bytecode"]["object"]
+            )
+            payload["source_map"] = result["contracts"][file][contract]["evm"][
+                "bytecode"
+            ]["sourceMap"]
+            payload["deployed_bytecode"] = patch_solc_bytecode(
+                result["contracts"][file][contract]["evm"]["deployedBytecode"]["object"]
+            )
+            payload["deployed_source_map"] = result["contracts"][file][contract]["evm"][
+                "deployedBytecode"
+            ]["sourceMap"]
+            return payload
+        except KeyError:
+            LOGGER.warning(
+                f"Could not find contract {contract} in compilation artifacts. The CLI will find the "
+                f"largest bytecode artifact in the compilation output and submit it instead."
+            )
+
+    # extract the largest bytecode from the compilation result and add it
     bytecode_max = 0
-    for index, (contract_name, contract_data) in enumerate(result.items()):
-        ast = contract_data["ast"]
-        source_path = str(Path(ast.get("attributes", {}).get("absolutePath")))
-        creation_bytecode = contract_data["bin"]
-        deployed_bytecode = contract_data["bin-runtime"]
-        source_map = contract_data["srcmap"]
-        deployed_source_map = contract_data["srcmap-runtime"]
-        with open(source_path) as source_f:
-            source = source_f.read()
-            LOGGER.debug(f"Loaded contract source with {len(source)} characters")
-
-        # always add source and AST, even if dependency
-        payload["sources"][source_path] = {"source": source, "ast": ast}
-        payload["source_list"].append(source_path)
-        if (contracts and contract_name not in contracts) or (
-            not contracts and len(creation_bytecode) < bytecode_max
-        ):
-            LOGGER.debug(f"Found dependency contract {contract_name} - continung")
-            continue
-
-        bytecode_max = len(creation_bytecode)
-        LOGGER.debug(f"Updaing main payload for {contract_name}")
-        payload.update(
-            {
-                "contract_name": contract_name,
-                "main_source": source_path,
-                "bytecode": patch_solc_bytecode(creation_bytecode),
-                "source_map": set_srcmap_indices(source_map, index),
-                "deployed_source_map": set_srcmap_indices(deployed_source_map, index),
-                "deployed_bytecode": patch_solc_bytecode(deployed_bytecode),
-            }
-        )
+    for file_path, file_element in result.get("contracts", {}).items():
+        for contract, contract_data in file_element.items():
+            contract_bytecode = contract_data["evm"]["bytecode"]["object"]
+            contract_source_map = contract_data["evm"]["bytecode"]["sourceMap"]
+            contract_deployed_bytecode = contract_data["evm"]["deployedBytecode"][
+                "object"
+            ]
+            contract_deployed_source_map = contract_data["evm"]["deployedBytecode"][
+                "sourceMap"
+            ]
+            bytecode_length = len(contract_bytecode)
+            if bytecode_length > bytecode_max:
+                bytecode_max = bytecode_length
+                payload["contract_name"] = contract
+                payload["bytecode"] = patch_solc_bytecode(contract_bytecode)
+                payload["source_map"] = contract_source_map
+                payload["deployed_bytecode"] = patch_solc_bytecode(
+                    contract_deployed_bytecode
+                )
+                payload["deployed_source_map"] = contract_deployed_source_map
 
     return payload
