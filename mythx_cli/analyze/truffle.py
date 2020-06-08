be4c3484b1998807dbeeaee87468c15e6f0a4da4
Dominik Muhs <dmuhs@protonmail.ch>
2020-06-08 17:38:01 +0200

    Order truffle job methods

--- mythx_cli/analyze/truffle.py
+++ mythx_cli/analyze/truffle.py
@@ -14,9 +14,50 @@ LOGGER = logging.getLogger("mythx-cli")
 
 
 class TruffleJob:
+    """A truffle job to be sent to the API.
+
+    This object represents a collection of truffle artifacts that will
+    be sent to the API. It aggregates artifacts and transforms them into
+    API-conform payload dicts.
+    """
+
     def __init__(self, target: Path):
         self.target = target
         self.payloads = []
+
+    def find_truffle_artifacts(
+        self
+    ) -> Union[Tuple[List[str], List[str]], Tuple[None, None]]:
+        """Look for a Truffle build folder and return all relevant JSON artifacts.
+
+        This function will skip the Migrations.json file and return all other files
+        under :code:`<project-dir>/build/contracts/`. If no files were found,
+        :code:`None` is returned.
+
+        :return: Files under :code:`<project-dir>/build/contracts/` or :code:`None`
+        """
+
+        output_pattern = self.target / "build" / "contracts" / "*.json"
+        artifact_files = list(glob(str(output_pattern.absolute())))
+        if not artifact_files:
+            LOGGER.debug(f"No truffle artifacts found in pattern {output_pattern}")
+            return None, None
+
+        sources: Set[Tuple[int, str]] = set()
+        for file in artifact_files:
+            with open(file) as af:
+                artifact = json.load(af)
+                try:
+                    ast = artifact.get("ast") or artifact.get("legacyAST")
+                    idx = ast.get("src", "").split(":")[2]
+                    sources.add((int(idx), artifact.get("sourcePath")))
+                except (KeyError, IndexError) as e:
+                    LOGGER.warning(f"Could not reconstruct artifact source list: {e}")
+                    sys.exit(1)
+
+        # infer source list from artifact collection
+        source_list = [x[1] for x in sorted(list(sources), key=lambda x: x[0])]
+        return artifact_files, source_list
 
     def generate_payloads(self):
         """Generate a MythX analysis request payload based on a truffle build
@@ -80,39 +121,6 @@ class TruffleJob:
                     "solc_version": artifact["compiler"]["version"],
                 }
             )
-
-    def find_truffle_artifacts(
-        self
-    ) -> Union[Tuple[List[str], List[str]], Tuple[None, None]]:
-        """Look for a Truffle build folder and return all relevant JSON artifacts.
-
-        This function will skip the Migrations.json file and return all other files
-        under :code:`<project-dir>/build/contracts/`. If no files were found,
-        :code:`None` is returned.
-
-        :return: Files under :code:`<project-dir>/build/contracts/` or :code:`None`
-        """
-
-        output_pattern = self.target / "build" / "contracts" / "*.json"
-        artifact_files = list(glob(str(output_pattern.absolute())))
-        if not artifact_files:
-            LOGGER.debug(f"No truffle artifacts found in pattern {output_pattern}")
-            return None, None
-
-        sources: Set[Tuple[int, str]] = set()
-        for file in artifact_files:
-            with open(file) as af:
-                artifact = json.load(af)
-                try:
-                    ast = artifact.get("ast") or artifact.get("legacyAST")
-                    idx = ast.get("src", "").split(":")[2]
-                    sources.add((int(idx), artifact.get("sourcePath")))
-                except (KeyError, IndexError) as e:
-                    LOGGER.warning(f"Could not reconstruct artifact source list: {e}")
-                    sys.exit(1)
-
-        source_list = [x[1] for x in sorted(list(sources), key=lambda x: x[0])]
-        return artifact_files, source_list
 
     @staticmethod
     def patch_truffle_bytecode(code: str) -> str:
