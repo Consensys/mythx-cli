cf358592ac197cb1a782de7922edd78f1ae4057a
Dominik Muhs <dmuhs@protonmail.ch>
2020-09-05 12:13:15 +0200

    Bump version: 0.6.19 → 0.6.20

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/dmuhs/mythx-cli",
-    version="0.6.19",
+    version="0.6.20",
     zip_safe=False,
 )
