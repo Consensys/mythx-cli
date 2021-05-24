51e4babec0bcbea0ab8cc867a3bccf07ed2a95ff
Nuradil <nurchik17@gmail.com>
2021-05-24 22:43:00 +0600

    Refactor IDE code

--- mythx_cli/fuzz/ide/truffle.py
+++ mythx_cli/fuzz/ide/truffle.py
@@ -170,48 +170,6 @@ class TruffleArtifacts(IDEArtifacts):
             )
         return contracts
 
-    @staticmethod
-    def _get_build_artifacts(build_dir) -> Dict:
-        """Build indexes of Truffle build artifacts.
-
-        This function starts by loading the contents from the Truffle build artifacts json, found in the /build
-        folder, which contain the following data:
-
-        * :code: `deployedSourceMap`
-        * :code: `deployedBytecode`
-        * :code: `sourceMap`
-        * :code: `bytecode`
-        * :code: `contractName`
-        * :code: `sourcePath`
-
-        It then stores that data in build_files_by_source_file dictionary. The one is indexed by the
-        source file (.sol) found in the .sourcePath property of the json.
-        """
-        build_files_by_source_file = {}
-
-        build_dir = Path(build_dir)
-
-        if not build_dir.is_dir():
-            raise BuildArtifactsError("Build directory doesn't exist")
-
-        for child in build_dir.glob("**/*"):
-            if not child.is_file():
-                continue
-            if not child.name.endswith(".json"):
-                continue
-
-            data = json.loads(child.read_text("utf-8"))
-
-            source_path = data["sourcePath"]
-
-            if source_path not in build_files_by_source_file:
-                # initialize the array of contracts with a list
-                build_files_by_source_file[source_path] = []
-
-            build_files_by_source_file[source_path].append(data)
-
-        return build_files_by_source_file
-
     @property
     def contracts(self):
         return self._contracts
